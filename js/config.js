422ac96fedae25c634b5533ba9c556b1b9feee5a
mhhung2 <126634318+mhhung2@users.noreply.github.com>
2026-08-28 01:02:56 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,4 @@
-const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbx1zD0KL6xRFbxXCxtyXUrwmggO-q1xkJRkkzH8er7cjcc9NhBRsiyga8BbNl_ZNkRc/exec";
+//const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbx1zD0KL6xRFbxXCxtyXUrwmggO-q1xkJRkkzH8er7cjcc9NhBRsiyga8BbNl_ZNkRc/exec";
+
+const GAS_WEB_APP_URL = "https://mhhung.jajajajajaja2001.workers.dev/";
+
